7cc46af3ce57a6d03264f3a4766a34a9db3c3139
Spencer Norman <spencerdnorman@gmail.com>
2022-07-07 14:59:37 -0500

    turbo stream is working

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,4 +4,9 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
+  broadcasts_to -> (quote) { "quotes" }, inserts_by: :prepend
+  # after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
 end
